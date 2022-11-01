e2be1f3c2e0d0d13fe453113c3753b1f91af3198
robertomalheiros <robertomalheirosb@gmail.com>
2022-11-01 11:06:32 -0300

    Elementos sorteados, Mudar posicionamento

--- src/index.js
+++ src/index.js
@@ -18,7 +18,8 @@ function Asteroide(reversa = false){
     imagem.src = images[ind]
     //INSERINDO A IMG DENTRO DA DIV BARREIRA
     this.elemento.appendChild(imagem)
-    this.setAltura = altura => imagem.style.position = `${altura}px`
+    this.setAltura = altura => imagem.style.top = `${altura}px`
+    console.log(this.setAltura())
 }
 
 //CHAMANDO TESTE DE ELEMENTOS
@@ -29,19 +30,25 @@ function Asteroide(reversa = false){
 function ParDeAsteroides(altura, abertura, x){
     this.elemento = novoElemento('div', 'par-asteroides')
 //Criando asteroide superior e inferior
-    this.superior = new Asteroide()
-    this.inferior = new Asteroide()
+    //this.superior = new Asteroide()
+    //this.inferior = new Asteroide()
+    this.meio = new Asteroide()
 
 //Adcionando elementos dentro de par-asteroides
-    this.elemento.appendChild(this.superior.elemento)
-    this.elemento.appendChild(this.inferior.elemento)
+    //this.elemento.appendChild(this.superior.elemento)
+    //this.elemento.appendChild(this.inferior.elemento)
+    this.elemento.appendChild(this.meio.elemento)
 //Função para sortear a abertura entre os asteroides
     this.sortearAbertura = () => {
-        const alturaSuperior = Math.random() * (altura - abertura)
-        console.log(alturaSuperior)
-        const alturaInferior = altura - abertura - alturaSuperior
-        this.superior.setAltura(alturaSuperior)
-        this.inferior.setAltura(alturaInferior) }
+        //const alturaSuperior = Math.random() * (altura - abertura)
+        //const alturaInferior = altura - abertura - alturaSuperior
+        //this.superior.setAltura(alturaSuperior)
+        //this.inferior.setAltura(alturaInferior)
+        const posicao = Math.random() * (altura - abertura)
+        console.log(posicao)
+
+        this.meio.setAltura(posicao)
+    }
 
     this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
     this.setX = x => this.elemento.style.left = `${x}px`
@@ -49,6 +56,8 @@ function ParDeAsteroides(altura, abertura, x){
     this.sortearAbertura()
     this.setX(x)
   }
-const b = new ParDeAsteroides(700, 100, 400)
+
+//TESTANDO PARES DE ASTEROIDES
+const b = new ParDeAsteroides(700, 300, 600)
 console.log(b.elemento)
 document.querySelector('#telagame').appendChild(b.elemento)
