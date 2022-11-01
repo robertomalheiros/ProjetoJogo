7dbe19cb0361f93dcf1f0e6dcc5e61c1f2cd5505
robertomalheiros <robertomalheirosb@gmail.com>
2022-11-01 19:39:40 -0300

    Pontuação com erro

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function novoElemento(tagName, className, src = false){
     const elem = document.createElement(tagName)
     elem.className = className
     return elem}
-//CRIANDO BARREIRAS A PARTIR DOS ELEMENTOS CRIANDO DA FUNÇÃO "novoElemento()"
+//CRIANDO Asteroides A PARTIR DOS ELEMENTOS CRIANDO DA FUNÇÃO "novoElemento()"
 function Asteroide(reversa = false){
     //LISTA ALEATORIA
     const images = ['./assets/1.png', './assets/2.png', './assets/3.png',
@@ -17,34 +17,23 @@ function Asteroide(reversa = false){
     //DEFININDO A IMG DENTRO DE ASTEROIDE
     const imagem = novoElemento('img', 'asteroideIMG')
     imagem.src = images[ind]
-    //INSERINDO A IMG DENTRO DA DIV BARREIRA
+    //INSERINDO A IMG DENTRO DA DIV 
     this.elemento.appendChild(imagem)
     this.setAltura = altura => imagem.style.top = `${altura}px`
 
 }
 
-//CHAMANDO TESTE DE ELEMENTOS
-  //const b = new Asteroide()
-  //document.querySelector('#telagame').appendChild(b.elemento)
 
-  //DEFININDO OS PARES DE ASTEROIDES
-function ParDeAsteroides(altura, abertura, x){
+  //DEFININDO OS GRUPO DE ASTEROIDES
+function GrupoAsteroides(altura, abertura, x){
     this.elemento = novoElemento('div', 'par-asteroides')
-//Criando asteroide superior e inferior
-    //this.superior = new Asteroide()
-    //this.inferior = new Asteroide()
+//Criando asteroide
     this.meio = new Asteroide()
 
 //Adcionando elementos dentro de par-asteroides
-    //this.elemento.appendChild(this.superior.elemento)
-    //this.elemento.appendChild(this.inferior.elemento)
     this.elemento.appendChild(this.meio.elemento)
 //Função para sortear a abertura entre os asteroides
     this.sortearAbertura = () => {
-        //const alturaSuperior = Math.random() * (altura - abertura)
-        //const alturaInferior = altura - abertura - alturaSuperior
-        //this.superior.setAltura(alturaSuperior)
-        //this.inferior.setAltura(alturaInferior)
         const posicao = Math.random() * (altura - abertura)
         this.meio.setAltura(posicao)
     }
@@ -56,33 +45,30 @@ function ParDeAsteroides(altura, abertura, x){
     this.setX(x)
   }
 
-//TESTANDO PARES DE ASTEROIDES
-//const b = new ParDeAsteroides(700, 300, 600)
-//console.log(b.elemento)
-//document.querySelector('#telagame').appendChild(b.elemento)
-function Barreiras(altura, largura, abertura, espaco, notificarPonto){
+
+function Asteroides(altura, largura, abertura, espaco, notificarPonto){
 
     this.grupo = [
-        new ParDeAsteroides(altura, abertura, largura),
-        new ParDeAsteroides(altura, abertura, largura + espaco),
-        new ParDeAsteroides(altura, abertura, largura + espaco * 2),
-        new ParDeAsteroides(altura, abertura, largura + espaco * 3 )
+        new GrupoAsteroides(altura, abertura, largura),
+        new GrupoAsteroides(altura, abertura, largura + espaco),
+        new GrupoAsteroides(altura, abertura, largura + espaco * 2),
+        new GrupoAsteroides(altura, abertura, largura + espaco * 3 )
     ]
 
     const deslocamento = 3
     this.animar = () => {
-        this.grupo.forEach(bar => {
-            bar.setX(bar.getX() - deslocamento)
+        this.grupo.forEach(ast => {
+            ast.setX(ast.getX() - deslocamento)
 
             //elemento saindo da tela
-            if(bar.getX() < -bar.getLargura()){
-                bar.setX(bar.getX() + espaco * this.grupo.length)
-                bar.sortearAbertura()
+            if(ast.getX() < -ast.getLargura()){
+                ast.setX(ast.getX() + espaco * this.grupo.length)
+                ast.sortearAbertura()
             }
 
             const cmeio = largura / 2 
-            const cruzou = bar.getX() + deslocamento >= cmeio &&
-            bar.getX() < cmeio
+            const cruzou = ast.getX() + deslocamento >= cmeio &&
+            ast.getX() < cmeio
             if(cruzou) notificarPonto()
         })
     }
@@ -122,15 +108,42 @@ function Nave(alturaJogo){
     }
     this.setY(alturaJogo / 2)
 }
-//TESTANDO MOVIMENTO
-const barreiras = new Barreiras(700, 1200, 200, 400)
-const area = document.querySelector('#telagame')
-barreiras.grupo.forEach(bar => area.appendChild(bar.elemento))
-const nave = new Nave(700)
-area.appendChild(nave.elemento)
 
+function Progresso(){
 
-setInterval(() => {
-    barreiras.animar()
-    nave.animar()
-}, 20)
+    this.elemento = novoElemento('span', 'progresso')
+    this.atualizarPontos = pontos => {
+    this.elemento.innerHTML = pontos
+    }
+    this.atualizarPontos(0)
+
+}
+
+function IronNave(){
+
+    let pontos = 0
+    const area = document.querySelector('#telagame')
+    const altura = area.clientHeight
+    const largura = area.clientLeft
+    const progresso = new Progresso()
+    const asteroides = new Asteroides(altura, largura, 200, 400, 
+        () => progresso.atualizarPontos(++pontos)   )
+    
+    const nave = new Nave(altura)
+
+    area.appendChild(nave.elemento)
+    area.appendChild(progresso.elemento)
+    asteroides.grupo.forEach(ast => area.appendChild(ast.elemento))
+    
+    this.start = () => {
+        const temp = setInterval(() => {
+
+            asteroides.animar()
+            nave.animar()
+            }, 20)
+    }
+
+}
+
+new IronNave().start()
+
