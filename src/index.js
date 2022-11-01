b1439ba06a849bbc10ddca583c7f92698b55ce84
robertomalheiros <robertomalheirosb@gmail.com>
2022-10-31 22:10:10 -0300

    Elemento Barreira aleatório

---
+++ src/index.js
@@ -0,0 +1,28 @@
+
+
+//LILSTA DE IMAGENS
+
+function novoElemento(tagName, className, src = false){
+    const elem = document.createElement(tagName)
+    elem.className = className
+    /*if(src = true){
+        const elem = document.createElement(tagName)
+        elem.className = className
+        elem.src = src
+        return elem
+    } */
+    return elem}
+
+function Barreira(reversa = false){
+    const images = ['./assets/0.png', './assets/1.png', './assets/2.png']
+    let ind = parseInt(Math.random() * images.length)
+    this.elemento = novoElemento('div', 'barreira')
+    const imagem = novoElemento('img', 'barreiraIMG')
+    imagem.src = images[ind]
+    this.elemento.appendChild(imagem)
+}
+
+//CRIANDO BARREIRAS A PARTIR DOS ELEMENTOS CRIANDO DA FUNÇÃO "novoElemento()"
+
+  const b = new Barreira()
+  document.querySelector('#telagame').appendChild(b.elemento)
